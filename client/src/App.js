1177997bd4f887c980de3bf575f85105dec73777
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-04-16 01:00:30 +0530

    add:responsive navbar with routing system

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import "./App.css";
+import React,{ useState } from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import{Home,About,Services,Products,Contact} from "./pages"
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <nav className="navbar">
+          <div className="navbar-toggle" onClick={toggleMenu}>
+            <div className={`bar ${isMenuOpen ? 'open' : ''}`}></div>
+            <div className={`bar ${isMenuOpen ? 'open' : ''}`}></div>
+            <div className={`bar ${isMenuOpen ? 'open' : ''}`}></div>
+          </div>
+          <ul className={`navbar-nav ${isMenuOpen ? 'open' : ''}`}>
+            <li className="nav-item">
+              <Link to="/" className="nav-link" onClick={() => setIsMenuOpen(false)}>Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link" onClick={() => setIsMenuOpen(false)}>About</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/services" className="nav-link" onClick={() => setIsMenuOpen(false)}>Services</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/products" className="nav-link" onClick={() => setIsMenuOpen(false)}>Products</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link" onClick={() => setIsMenuOpen(false)}>Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" exact element={<Home/>}/>
+          <Route path="/about" exact element={<About />}/>
+          <Route path="/services" exact element={<Services />}/>
+          <Route path="/products" exact element={<Products />}/>
+          <Route path="/contact" exact element={<Contact/>}/>
+        </Routes>
+      </div>
   );
 }
 
